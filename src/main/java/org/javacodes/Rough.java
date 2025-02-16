3c21107744d388c7b802761c5475ea2a7bbc0711
raviprasadchowdhary <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-02-16 13:44:53 +0530

    refactor

--- src/main/java/org/javacodes/Rough.java
+++ src/main/java/org/javacodes/Rough.java
@@ -1,4 +1,43 @@
 package org.javacodes;
 
+import org.javacodes.helper.Helper;
+
 public class Rough {
+    public static void main(String[] args) {
+        String input = Helper.readSingleInputString();
+
+        System.out.println("Input string before reversing: " + input);
+
+        System.out.println("Reversed string by StringBuilder: " + reverseStringByStringBuilder(input));
+        System.out.println("Reversed string by concatenation: " + reverseStringByConcatenation(input));
+        System.out.println("Reversed string by character array: " + reverseStringByCharArray(input));
+    }
+
+    // method1: using StringBuilder or StringBuffer
+    private static String reverseStringByStringBuilder(String input) {
+        return new StringBuilder(input).reverse().toString();
+    }
+
+    // method2: using string concatenation
+    private static String reverseStringByConcatenation(String input) {
+        String reversedString = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversedString += input.charAt(i);
+        }
+        return reversedString;
+    }
+
+    // method3: using character array
+    private static String reverseStringByCharArray(String input) {
+        char[] charArray = input.toCharArray();
+        int left = 0, right = charArray.length - 1;
+        while (left < right) {
+            char temp = charArray[left];
+            charArray[left] = charArray[right];
+            charArray[right] = temp;
+            left++;
+            right--;
+        }
+        return new String(charArray);
+    }
 }
