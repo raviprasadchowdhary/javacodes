b618af919e74600c6c1dc92ee6ce4db2b8f7618f
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-03-02 00:21:20 +0530

    org.javacodes.core.pattern.FloydTriangle

---
+++ src/main/java/org/javacodes/core/pattern/FloydTriangle.java
@@ -0,0 +1,18 @@
+package org.javacodes.core.pattern;
+
+import org.javacodes.helper.Helper;
+
+public class FloydTriangle {
+    public static void main(String[] args){
+        int rows = Helper.readSingleInputInteger();
+
+        int count = 1;
+        for (int i=1; i<=rows; i++){
+            for (int j=1; j<=i; j++){
+                System.out.print(count + " ");
+                count++;
+            }
+            System.out.println();
+        }
+    }
+}
