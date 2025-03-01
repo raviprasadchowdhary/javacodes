ba3af3ae336ccf2b9923b39f7ea642a2a25fbfd7
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-03-02 00:00:35 +0530

    org.javacodes.core.pattern.Block

---
+++ src/main/java/org/javacodes/core/pattern/Block.java
@@ -0,0 +1,13 @@
+package org.javacodes.core.pattern;
+
+public class Block {
+    public static void main(String[] args){
+        int size = 5;
+        for (int i =0; i<size; i++){
+            for (int j=0; j<size; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
