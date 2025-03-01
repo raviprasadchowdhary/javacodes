e1c664d276c7dd4bdfb877edc37e8253235cf60a
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-03-02 00:12:09 +0530

    org.javacodes.core.pattern.RightAngledTriangle

---
+++ src/main/java/org/javacodes/core/pattern/RightAngledTriangle.java
@@ -0,0 +1,16 @@
+package org.javacodes.core.pattern;
+
+import org.javacodes.helper.Helper;
+
+public class RightAngledTriangle {
+    public static void main(String[] args) {
+        int size = Helper.readSingleInputInteger();
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
