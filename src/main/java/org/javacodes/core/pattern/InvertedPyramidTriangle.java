ab5bc15ef2b1d7b299441d07381aa354f334cb58
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-03-02 11:32:31 +0530

    org.javacodes.core.pattern.InvertedPyramidTriangle

---
+++ src/main/java/org/javacodes/core/pattern/InvertedPyramidTriangle.java
@@ -0,0 +1,20 @@
+package org.javacodes.core.pattern;
+
+import org.javacodes.helper.Helper;
+
+public class InvertedPyramidTriangle {
+	public static void main(String[] args) {
+		int row = Helper.readSingleInputInteger();
+
+		System.out.println("Below is the inverted pyramid triangle with " + row + " rows");
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = ((row - i - 1) * 2) + 1; j > 0; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
